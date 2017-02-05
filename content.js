33745b8614bacd660e689966016e4733cec7bb43
Kozlovsky Anton <kozlovsky.anton@gmail.com>
2017-02-05 13:03:23 +0200

    start logic

--- content.js
+++ content.js
@@ -1,3 +1,43 @@
 /**
  * Created by Anton on 04.02.2017.
  */
+console.log("работаю");
+
+var node = document.getElementsByClassName("f_post post page_block all own");
+
+
+
+console.log("-> " + node);
+console.log(document.body);
+
+
+
+
+for (var i = 0; i < node.length; i++) {
+    console.log("->"+node[i]);
+    if (node[i].parentNode) {
+        node[i].parentNode.removeChild(node[i]);
+        console.log("тип удалил")
+    }
+    else {
+        console.log("not parent");
+    }
+}
+
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    // If the received message has the expected format...
+    console.log("!!!!!!!!!!!!!!!");
+
+
+    if (msg.text === 'report_back') {
+        // Call the specified callback, passing
+        // the web-page's DOM content as argument
+        console.log("uhudkfhvsfhvljdfv???");
+
+
+
+
+        sendResponse(document.all[0].outerHTML);
+    }
+});
