05891407e510dc37c93943e6421f0e6624d51d92
Nadine Astakhova <nadine.astakhova@gmail.com>
2017-02-05 15:48:20 +0200

    Textarea

--- content.js
+++ content.js
@@ -1,4 +1,12 @@
 /**
  * Created by Anton on 04.02.2017.
  */
-
+$(document).ready(function(){
+    jQuery.each(jQuery('textarea[data-autoresize]'), function() {
+    var offset = this.offsetHeight - this.clientHeight;
+ 
+    var resizeTextarea = function(el) {
+        jQuery(el).css('height', 'auto').css('height', el.scrollHeight + offset);
+    };
+    jQuery(this).on('keyup input', function() { resizeTextarea(this); }).removeAttr('data-autoresize');
+});});
